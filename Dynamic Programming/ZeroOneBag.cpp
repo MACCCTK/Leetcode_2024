9c22f8e170a6f3a84563a1ec71c0e67f2e8f7f80
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-16 20:07:28 +0800

    Update and rename 01__Bag to ZeroOneBag.cpp

--- Dynamic Programming/ZeroOneBag.cpp
+++ Dynamic Programming/ZeroOneBag.cpp
@@ -1,26 +1,11 @@
 在背包问题中，遍历物品和背包容量的顺序会影响动态规划表的填充顺序：
-
     先遍历物品：我们对每个物品 i，计算其在不同容量 j 下的最优解。这种方式直观且易于理解，因为它逐步考虑每个物品是否放入背包。
-
     先遍历背包容量：对于每个容量 j，我们考虑不同物品 i 的最优解。这种方式在解决完全背包问题时常用，在01背包问题中需要适当调整。
 
-问能否能装满背包（或者最多装多少）：dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]); ，对应题目如下：
-
-动态规划：416.分割等和子集
-动态规划：1049.最后一块石头的重量 II
-问装满背包有几种方法：dp[j] += dp[j - nums[i]] ，对应题目如下：
-
-动态规划：494.目标和
-动态规划：518. 零钱兑换 II
-动态规划：377.组合总和Ⅳ
-动态规划：70. 爬楼梯进阶版（完全背包）
-问背包装满最大价值：dp[j] = max(dp[j], dp[j - weight[i]] + value[i]); ，对应题目如下：
-
-动态规划：474.一和零
-问装满背包所有物品的最小个数：dp[j] = min(dp[j - coins[i]] + 1, dp[j]); ，对应题目如下：
-
-动态规划：322.零钱兑换
-动态规划：279.完全平方数
+问能否能装满背包（或者最多装多少）：dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+问装满背包有几种方法：dp[j] += dp[j - nums[i]] ;
+问背包装满最大价值：dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+问装满背包所有物品的最小个数：dp[j] = min(dp[j - coins[i]] + 1, dp[j]);
 
 
 void test_2_wei_bag_problem1() {
@@ -28,15 +13,11 @@ void test_2_wei_bag_problem1() {
     vector<int> value = {15, 20, 30};
     int bagweight = 4;
 
-    // 二维数组
     vector<vector<int>> dp(weight.size(), vector<int>(bagweight + 1, 0));
-
-    // 初始化
     for (int j = weight[0]; j <= bagweight; j++) {
         dp[0][j] = value[0];
     }
 
-    // weight数组的大小 就是物品个数
     for(int i = 1; i < weight.size(); i++) { // 遍历物品
         for(int j = 0; j <= bagweight; j++) { // 遍历背包容量
             if (j < weight[i]) dp[i][j] = dp[i - 1][j];
@@ -52,31 +33,17 @@ int main() {
     test_2_wei_bag_problem1();
 }
 
-
-
 假设有3个物品，重量分别为 [2, 3, 4]，价值分别为 [3, 4, 5]，背包容量为 5。
 
 动态规划表初始状态：
-
-
 dp[0] = [0, 0, 0, 0, 0, 0]
-
 填表过程：
-
     物品 1 （重量 2，价值 3）：
-
-
 dp[1] = [0, 0, 3, 3, 3, 3]
-
     物品 2 （重量 3，价值 4）：
-
-
 dp[2] = [0, 0, 3, 4, 4, 7]
-
     物品 3 （重量 4，价值 5）：
-
 dp[3] = [0, 0, 3, 4, 5, 7]
-
 最终 dp 表示在不同背包容量下的最优解，dp[n][bagweight] 就是所求的最大价值。
 
 
@@ -85,7 +52,6 @@ void test_1_wei_bag_problem() {
     vector<int> value = {15, 20, 30};
     int bagWeight = 4;
 
-    // 初始化
     vector<int> dp(bagWeight + 1, 0);
     for(int i = 0; i < weight.size(); i++) { // 遍历物品
         for(int j = bagWeight; j >= weight[i]; j--) { // 遍历背包容量
@@ -98,4 +64,3 @@ void test_1_wei_bag_problem() {
 int main() {
     test_1_wei_bag_problem();
 }
-

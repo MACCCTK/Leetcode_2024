47d7f532de92adeca0f7fe4cfaed00ee2a537a30
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-19 21:32:13 +0800

    Create 188+309+714_maxProfitLimits.cpp

---
+++ Dynamic Programming/188+309+714_maxProfitLimits.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0) return 0;
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] -= prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][3] - prices[i], dp[i - 1][1] - prices[i]));//持有股票状态（今天买入股票，或者是之前买入股票然后没有操作）
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][3]);//保持卖出股票的状态
+            dp[i][2] = dp[i - 1][0] + prices[i];//今天卖出股票
+            dp[i][3] = dp[i - 1][2];//冷冻期
+        }
+        return max(dp[n - 1][3], max(dp[n - 1][1], dp[n - 1][2]));
+    }
+};
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+
+        if (prices.size() == 0) return 0;
+        vector<vector<int>> dp(prices.size(), vector<int>(2 * k + 1, 0));//k次买卖最多2k+1种状态
+        for (int j = 1; j < 2 * k; j += 2) {
+            dp[0][j] = -prices[0];
+        }
+        for (int i = 1;i < prices.size(); i++) {
+            for (int j = 0; j < 2 * k - 1; j += 2) {
+                dp[i][j + 1] = max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
+                dp[i][j + 2] = max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
+            }
+        }
+        return dp[prices.size() - 1][2 * k];
+    }
+};
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[0][0] -= prices[0]; 
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);//减去手续费
+        }
+        return max(dp[n - 1][0], dp[n - 1][1]);
+    }
+};

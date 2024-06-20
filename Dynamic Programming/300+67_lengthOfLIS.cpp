c0e3daf91201b0c07d118d1fec214f39c746e0e3
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-20 20:12:13 +0800

    Create 300+67_lengthOfLIS.cpp

---
+++ Dynamic Programming/300+67_lengthOfLIS.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if (nums.size() <= 1) return nums.size();
+        vector<int> dp(nums.size(), 1);
+        int result = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) dp[i] = max(dp[i], dp[j] + 1);
+            }
+            if (dp[i] > result) result = dp[i]; // 取长的子序列
+        }
+        return result;
+    }
+};
+
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int result = 1;
+        vector<int> dp(nums.size() ,1);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+            if (dp[i] > result) result = dp[i];
+        }
+        return result;
+    }
+};

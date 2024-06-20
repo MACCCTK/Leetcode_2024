be31ec695d0c8dee77948caf942977e694392a4a
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-20 20:15:12 +0800

    Update 96_numTrees.cpp

--- Dynamic Programming/96_numTrees.cpp
+++ Dynamic Programming/96_numTrees.cpp
@@ -5,7 +5,7 @@ public:
         dp[0] = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                dp[i] += dp[j - 1] * dp[i - j];
+                dp[i] += dp[j - 1] * dp[i - j];//j-1 为j为头结点左子树节点数量，i-j 为以j为头结点右子树节点数量
             }
         }
         return dp[n];

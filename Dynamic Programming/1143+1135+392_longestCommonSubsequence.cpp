9b33eb65e2fde3dd35784d9b3a571399c47489df
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-21 21:42:30 +0800

    Update and rename 1143+1135_longestCommonSubsequence.cpp to 1143+1135+392_longestCommonSubsequence.cpp

--- Dynamic Programming/1143+1135+392_longestCommonSubsequence.cpp
+++ Dynamic Programming/1143+1135+392_longestCommonSubsequence.cpp
@@ -31,3 +31,18 @@ public:
         return dp[A.size()][B.size()];
     }
 };
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 1; j <= t.size(); j++) {
+                if (s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = dp[i][j - 1];//只能从左推
+            }
+        }
+        if (dp[s.size()][t.size()] == s.size()) return true;
+        return false;
+    }
+};

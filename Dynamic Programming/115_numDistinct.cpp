fd97198b3cc5dfb5238478de97ace82b656771c2
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-22 17:57:58 +0800

    Update 115_numDistinct.cpp

--- Dynamic Programming/115_numDistinct.cpp
+++ Dynamic Programming/115_numDistinct.cpp
@@ -9,7 +9,7 @@ public:
                 if (s[i - 1] == t[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
                 } else {
-                    dp[i][j] = dp[i - 1][j];//只能从上推
+                    dp[i][j] = dp[i - 1][j];//只能从上推/只能删除s串中的元素
                 }
             }
         }

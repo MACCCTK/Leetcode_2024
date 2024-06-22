072712c4005fe5034f4d52d0f1ff68d0271a1872
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-22 20:44:35 +0800

    Create 647+516_countSubstrings.cpp

---
+++ Dynamic Programming/647+516_countSubstrings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int countSubstrings(string s) {//连续串
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        int result = 0;
+        for (int i = s.size() - 1; i >= 0; i--) {  // 来自左下
+            for (int j = i; j < s.size(); j++) {//j一定是大于等于i的，在填充dp[i][j]时只填充右上半部分
+                if (s[i] == s[j]) {
+                    if (j - i <= 1) {
+                        result++;
+                        dp[i][j] = true;
+                    } else if (dp[i + 1][j - 1]) {
+                        result++;
+                        dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {//非连续数列
+        vector<vector<int>> dp(s.size(), vector<int>(s.size(), 0));
+        for (int i = 0; i < s.size(); i++) dp[i][i] = 1;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < s.size(); j++) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.size() - 1];
+    }
+};

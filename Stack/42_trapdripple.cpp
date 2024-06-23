0bc2b13259ba3cbd82b2083c2c0afb3082b63bfe
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-23 18:15:35 +0800

    Create 42_trapdripple.cpp

---
+++ Stack/42_trapdripple.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int> st;
+        st.push(0);
+        int sum = 0;
+        for (int i = 1; i < height.size(); i++) {
+            while (!st.empty() && height[i] > height[st.top()]) {
+                int mid = st.top();//最低点
+                st.pop();
+                if (!st.empty()) {
+                    int h = min(height[st.top()], height[i]) - height[mid];
+                    int w = i - st.top() - 1;
+                    sum += h * w;
+                }
+            }
+            st.push(i);
+        }
+        return sum;
+    }
+};
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.size() <= 2) return 0;
+        vector<int> maxLeft(height.size(), 0);
+        vector<int> maxRight(height.size(), 0);
+        int size = maxRight.size();
+
+        // 记录每个柱子左边柱子最大高度
+        maxLeft[0] = height[0];
+        for (int i = 1; i < size; i++) {
+            maxLeft[i] = max(height[i], maxLeft[i - 1]);
+        }
+        // 记录每个柱子右边柱子最大高度
+        maxRight[size - 1] = height[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            maxRight[i] = max(height[i], maxRight[i + 1]);
+        }
+        // 求和
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            int count = min(maxLeft[i], maxRight[i]) - height[i];
+            if (count > 0) sum += count;
+        }
+        return sum;
+    }
+};

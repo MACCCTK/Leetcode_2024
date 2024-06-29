433d7e29ee1113ff2bd4638b174fe35078b00444
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-06-29 20:57:23 +0800

    Create TopologicalSort.cpp

---
+++ Graph/TopologicalSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+int main() {
+    int m, n, s, t;
+    cin >> n >> m;
+    vector<int> inDegree(n, 0); 
+
+    unordered_map<int, vector<int>> umap;// 记录文件依赖关系
+    vector<int> result; 
+
+    while (m--) {
+        cin >> s >> t;
+        inDegree[t]++; 
+        umap[s].push_back(t); 
+    }
+    queue<int> que;
+    for (int i = 0; i < n; i++) {
+        if (inDegree[i] == 0) que.push(i);
+    }
+
+    while (que.size()) {
+        int  cur = que.front(); 
+        que.pop();
+        result.push_back(cur);
+        vector<int> files = umap[cur];
+        if (files.size()) {
+            for (int i = 0; i < files.size(); i++) {
+                inDegree[files[i]] --;
+                if(inDegree[files[i]] == 0) que.push(files[i]);
+            }
+        }
+    }
+    if (result.size() == n) {
+        for (int i = 0; i < n - 1; i++) cout << result[i] << " ";
+        cout << result[n - 1];
+    } else cout << -1 << endl;
+
+
+}

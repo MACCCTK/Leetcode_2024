12bf78e64b9dd1f15521a427d3a1b25c3cc12dca
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-07-08 08:04:12 +0800

    Create 941_validMountainArray.cpp

---
+++ Array/941_validMountainArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        if (A.size() < 3) return false;
+        int left = 0;
+        int right = A.size() - 1;
+
+        while (left < A.size() - 1 && A[left] < A[left + 1]) left++;
+        while (right > 0 && A[right] < A[right - 1]) right--;
+        if (left == right && left != 0 && right != A.size() - 1) return true;
+        return false;
+    }
+};

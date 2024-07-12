9f3a0060737ca3829fc477213de9725e7d3e83d6
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-07-12 17:54:43 +0800

    Create 922_sortArrayByParityII.cpp

---
+++ Array/922_sortArrayByParityII.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        vector<int> even(A.size() / 2); // 初始化就确定数组大小，节省开销
+        vector<int> odd(A.size() / 2);
+        vector<int> result(A.size());
+        int evenIndex = 0;
+        int oddIndex = 0;
+        int resultIndex = 0;
+        // 把A数组放进偶数数组，和奇数数组
+        for (int i = 0; i < A.size(); i++) {
+            if (A[i] % 2 == 0) even[evenIndex++] = A[i];
+            else odd[oddIndex++] = A[i];
+        }
+        // 把偶数数组，奇数数组分别放进result数组中
+        for (int i = 0; i < evenIndex; i++) {
+            result[resultIndex++] = even[i];
+            result[resultIndex++] = odd[i];
+        }
+        return result;
+    }
+};
+
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        int oddIndex = 1;
+        for (int i = 0; i < A.size(); i += 2) {
+            if (A[i] % 2 == 1) { // 在偶数位遇到了奇数
+                while(A[oddIndex] % 2 != 0) oddIndex += 2; // 在奇数位找一个偶数
+                swap(A[i], A[oddIndex]); // 替换
+            }
+        }
+        return A;
+    }
+};

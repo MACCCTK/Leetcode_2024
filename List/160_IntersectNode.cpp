ed78f72b04e9fb3c3c6d2c0129942d53e103447b
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-07-14 19:16:04 +0800

    Update 160_IntersectNode.cpp

--- List/160_IntersectNode.cpp
+++ List/160_IntersectNode.cpp
@@ -1,11 +1,36 @@
+//给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+
+//S1 双指针解法
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode *A = headA, *B = headB;
         while (A != B) {
-            A = A != nullptr ? A->next : headB;
-            B = B != nullptr ? B->next : headA;
+            A = A != nullptr ? A->next : headB; //如果 A 不等于 nullptr，则将 A 赋值为 A->next；否则，将 A 赋值为 headB。
+            B = B != nullptr ? B->next : headA; 
         }
         return A;
     }
 };
+
+//S2 哈希解法
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode *> visited;
+        ListNode *temp = headA;
+        while (temp != nullptr) {
+            visited.insert(temp);
+            temp = temp->next;
+        }
+        temp = headB;
+        while (temp != nullptr) {
+            if (visited.count(temp)) {
+                return temp;
+            }
+            temp = temp->next;
+        }
+        return nullptr;
+    }
+};
+

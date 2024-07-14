06c15ad2e94e8de4f440a6b6a16b2e865b918ab0
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-07-14 19:26:02 +0800

    Create 234_isPalindrome.cpp

---
+++ List/234_isPalindrome.cpp
@@ -0,0 +1,73 @@
+// 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+
+//数组双指针
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> vals;
+        while (head != nullptr) {
+            vals.emplace_back(head->val);
+            head = head->next;
+        }
+        for (int i = 0, j = (int)vals.size() - 1; i < j; ++i, --j) {
+            if (vals[i] != vals[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+// 快慢指针
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr) {
+            return true;
+        }
+
+        // 找到前半部分链表的尾节点并反转后半部分链表
+        ListNode* firstHalfEnd = endOfFirstHalf(head);
+        ListNode* secondHalfStart = reverseList(firstHalfEnd->next);
+
+        // 判断是否回文
+        ListNode* p1 = head;
+        ListNode* p2 = secondHalfStart;
+        bool result = true;
+        while (result && p2 != nullptr) {
+            if (p1->val != p2->val) {
+                result = false;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }        
+
+        // 还原链表并返回结果
+        firstHalfEnd->next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    ListNode* endOfFirstHalf(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
+

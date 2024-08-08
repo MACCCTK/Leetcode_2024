801fd68b1bf90c993b742c78851236296c3303ae
Carlos Cui <125531758+MACCCTK@users.noreply.github.com>
2024-08-08 19:29:56 +0800

    Update and rename 739_dailyTemperatures.cpp to 739*_dailyTemperatures.cpp

--- Stack/739*_dailyTemperatures.cpp
+++ Stack/739*_dailyTemperatures.cpp
@@ -1,3 +1,6 @@
+//给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+//2024_0808 第一次回顾
+
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& T) {
@@ -21,6 +24,11 @@ public:
     }
 };
 
+//时间复杂度：O(n)，其中 n 是温度列表的长度。正向遍历温度列表一遍，对于温度列表中的每个下标，最多有一次进栈和出栈的操作。
+//空间复杂度：O(n)，其中 n 是温度列表的长度。需要维护一个单调栈存储温度列表中的下标。
+
+
+//简化版本：
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& T) {
